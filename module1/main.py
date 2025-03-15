25482fe9b508daa8d527a6e4abf5aae424bd75df
Ramesh Nookala <73360820+ramariba@users.noreply.github.com>
2025-03-15 11:30:21 -0400

    Update main.py

--- module1/main.py
+++ module1/main.py
@@ -9,9 +9,9 @@ os.environ['GOOGLE_API_KEY'] = st.secrets['GOOGLE_API_KEY']
 
 # Create prompt template for generating tweets
 
-tweet_template = "Give me {number} tweets on {topic}"
+tweet_template = "Give me {number} jokes on {topic} in {language} language"
 
-tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic', 'language'])
 
 # Initialize Google's Gemini model
 gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
@@ -23,15 +23,18 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("🐦 Ramesh Nookala - Tweet Generator")
+st.header("🐦 Ramesh Nookala - Joke Generator")
 
-st.subheader("Generate tweets using Generative AI 🤖")
+st.subheader("Generate Jokes using Generative AI 🤖")
 
 topic = st.text_input("Topic")
 
 number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 
+language_options = ["English", "Hindi", "Telugu"]
+language = st.selectbox("Select a language:", language_options)
+
 if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
+    tweets = tweet_chain.invoke({"number" : number, "topic" : topic, "language" : language})
     st.write(tweets.content)
     
